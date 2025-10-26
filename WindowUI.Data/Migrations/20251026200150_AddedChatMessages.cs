5eac05f85f9fb9a472c33d0e21447e908119be91
XanderVanDerWeken <vanxa812@yahoo.de>
2025-10-26 21:05:32 +0100

    Adding Chat Migration

---
+++ WindowUI.Data/Migrations/20251026200150_AddedChatMessages.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WindowUI.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedChatMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatMessages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AuthorName = table.Column<string>(type: "TEXT", nullable: false),
+                    Message = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ChatMessages_AuthorName",
+                table: "ChatMessages",
+                column: "AuthorName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ChatMessages");
+        }
+    }
+}
